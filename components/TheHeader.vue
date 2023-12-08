91bfbd12ba7c36babb4643554958b09c9160e5a5
rafaeltj13 <rafael.damaciel@gmail.com>
2023-12-08 07:58:21 -0300

    initial commit

---
+++ components/TheHeader.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="bg-primary w-full h-20 shadow-xl z-10 relative"></div>
+</template>
---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <TheHeader />
+  <div class="flex items-start justify-between h-[calc(100vh-4rem)] shadow-md">
+    <div class="w-full h-full bg-slate-200 p-10 text-slate-700">
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script setup>
+const ui = /*ui*/ {
+  background: "bg-white dark:bg-red-900",
+};
+</script>

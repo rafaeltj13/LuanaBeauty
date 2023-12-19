08d6855479769e8b72df18df0eb761a0f6483ca3
Rafael Maciel <rafael.damaciel@gmail.com>
2023-12-19 07:46:22 -0300

    feat: added opacity to header

--- components/TheHeader.vue
+++ components/TheHeader.vue
@@ -1,19 +1,11 @@
 <template>
   <div
-    :class="{ 'shadow-xl': !isScrollAtTop }"
+    :class="{ 'shadow-xl opacity-90': !isScrollAtTop }"
     class="bg-primary w-full h-16 z-10 flex items-center fixed"
   >
     <div
       class="w-full max-w-[1160px] mx-auto flex items-center justify-between xl:px-0 px-4 md:px-12"
     >
-      <UButton
-        icon="i-heroicons-list-bullet"
-        size="md"
-        color="emerald"
-        variant="link"
-        trailing
-        class="md:hidden flex"
-      />
       <p
         class="text-4xl cursor-pointer text-secondary"
         @click="navigateTo('/')"
@@ -39,14 +31,22 @@
         trailing
         class="hidden md:flex"
       />
-      <UButton
-        icon="i-heroicons-pencil-square"
-        size="md"
-        color="emerald"
-        variant="solid"
-        trailing
-        class="md:hidden flex"
-      />
+      <div class="flex items-center gap-4 md:hidden">
+        <UButton
+          icon="i-heroicons-list-bullet"
+          size="md"
+          color="emerald"
+          variant="link"
+          trailing
+        />
+        <UButton
+          icon="i-heroicons-pencil-square"
+          size="md"
+          color="emerald"
+          variant="solid"
+          trailing
+        />
+      </div>
     </div>
   </div>
 </template>
--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,10 @@
 <template>
-  <TheHeader />
-  <div class="flex items-start justify-between h-[100vh] shadow-md pt-16">
-    <div class="w-full h-full bg-slate-200 text-slate-700">
-      <slot />
+  <div class="bg-primary">
+    <TheHeader />
+    <div class="flex items-start justify-between h-[100vh] shadow-md pt-16">
+      <div class="w-full h-full bg-slate-200 text-slate-700">
+        <slot />
+      </div>
     </div>
   </div>
 </template>
